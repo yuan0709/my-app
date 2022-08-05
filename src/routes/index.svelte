abcd92911475cd633ea452ceec6958215abfcf02
yuan0709 <hnnzhng@gmail.com>
2022-08-05 10:54:40 +0800

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script >
+let count =0;
+function upcount(){
+	count += 1;
+}
+function downcount(){
+	count -= 1;
+}
+</script>
+
+{#if count ==1 }
+	<h1>
+		i have {count} dollor
+	</h1>
+{:else}
+	<h1>
+		i have {count} dollors
+	</h1>
+{/if}
+
+<button on:click={upcount}>
+	+1
+</button>
+<button on:click={downcount}>
+	-1
+</button>
